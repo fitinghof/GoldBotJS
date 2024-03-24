d6d7ea59e40ab330644af504347c17353b85db80
Simon Nilsson <simon.f.nilsson@me.com>
2024-03-24 21:45:36 +0100

    started working on a shittyDatabase

--- shittyDatabase.js
+++ shittyDatabase.js
@@ -1,27 +1,59 @@
+const { Collection } = require("discord.js");
 const fs = require("fs")
+const path = require('path');
+
 
 class DataBase {
-    #filePath;
-    #data;
+    #directoryPath;
+    data = {};
     constructor(filePath) {
-        dataPath = fs.readdirSync(path.join(__dirname, 'persistantData'))
+        this.#directoryPath = filePath;
+/*         const dataPath = fs.readdirSync(this.#directoryPath)
         for (const file of dataPath) {
-            const filePath = path.join(dataPath, file);
-            fs.readFile(filePath, (err, content) => {
-                if (err) {
-                    console.error('Error reading file:', err);
-                    return;
-                }
-                const dataName = file.replace((symbol) => {symbol === ".json"}, "")
-                const data = JSON.parse(content);
-                client.data[dataName] = data;
-                client.data[dataName].save = () => {};
-            })
+            const filePath = path.join(this.#directoryPath, file);
+            const dataString = fs.readFileSync(filePath, 'utf8');
+            const data = JSON.parse(dataString);
+            const dataName = file.replace(".json", "")
+            const dataCollection = new Collection()
+            for(const key in data){
+                dataCollection.set(key, converter(data[key]))
+            }
+            this.data[dataName] = {}
+            this.data[dataName] = dataCollection;
+            this.data[dataName].save = () => {
+                const newData = JSON.stringify(Object.fromEntries(this.data[dataName]), null, 2);
+                    fs.writeFile(path.join(this.#directoryPath, dataName, ".json"), newData, (err) => {
+                        if(err) console.error(err);
+                    })
+                };
         } 
+    */
+}
+    SyncData(dataName, converter = function (entry) { return entry }) {
+        const filePath = path.join(this.#directoryPath, dataName + ".json");
         const dataString = fs.readFileSync(filePath, 'utf8');
-        this.data = JSON.parse(dataString);
+        const data = JSON.parse(dataString);
+        const dataCollection = new Collection()
+        for(const key in data){
+            dataCollection.set(key, converter(data[key]))
+        }
+        this.data[dataName] = dataCollection;
+        this.data[dataName].save = () => {
+            const newData = JSON.stringify(Object.fromEntries(this.data[dataName]), null, 2);
+                fs.writeFile(path.join(this.#directoryPath, dataName + ".json"), newData, (err) => {
+                    if(err) console.error(err);
+                })
+            };
+        this.data[dataName].noSaveSet = this.data[dataName].set
+        this.data[dataName].set = (key, value) => {
+            this.data[dataName].noSaveSet(key, value);
+            this.data[dataName].save();
+        }
     }
 }
-module.exports = {
 
-}
+const dataBase = new DataBase(__dirname + "/persistantData");
+dataBase.SyncData("otherData");
+dataBase.data.otherData.set("jackPot", 100);
+console.log(dataBase.data.otherData);
+//console.log(JSON.stringify(dataBase, null, 2));
